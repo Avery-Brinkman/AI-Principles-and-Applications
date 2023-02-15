969c46946b1fc9b314c842e3693ede30b1e2693d
Avery Brinkman <averybrinkman@gmail.com>
2023-02-15 11:54:30 -0500

    read from file

--- HW2/src/main.cpp
+++ HW2/src/main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 #include "GameBoard.h"
 
 using namespace GAME_NS;
 
-int main() {
-  std::vector<GameBoard> homeworkInputs = {
-      {9, 4, {{1, 7}, {2, 4}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {9, 5, {{1, 7}, {2, 4}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {9, 6, {{1, 7}, {2, 4}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {10, 3, {{1, 7}, {2, 4}, {2, 5}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {10, 4, {{1, 7}, {2, 4}, {2, 5}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {10, 5, {{1, 7}, {2, 4}, {2, 5}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}},
-      {10, 6, {{1, 7}, {2, 4}, {2, 5}, {3, 8}, {4, 1}, {4, 6}, {5, 5}, {6, 2}, {7, 2}, {8, 3}}}};
+std::vector<std::string> parse(std::string& str, std::string delim) {
+  size_t pos = 0;
+  std::string token;
+  std::vector<std::string> out = {};
+  while ((pos = str.find(delim)) != std::string::npos) {
+    token = str.substr(0, pos);
+    out.push_back(token);
+    str.erase(0, pos + delim.length());
+  }
+  out.push_back(str);
+  return out;
+}
 
-  for (auto b : homeworkInputs) {
-    b.solve();
+int main() {
+  std::string numberOfQueens, range, positionsLine;
+  std::vector<GameBoard> inputs = {};
+
+  std::ifstream inputFile("------------PATH/TO/FILE-------------");
+
+  if (inputFile.is_open()) {
+    while (!inputFile.eof()) {
+      std::vector<Queen> queens = {};
+
+      // Get values from file
+      std::getline(inputFile, numberOfQueens);
+      std::getline(inputFile, range);
+      std::getline(inputFile, positionsLine);
+
+      std::vector<std::string> positionsList = parse(positionsLine, " ");
+      for (std::string positionString : positionsList) {
+        std::vector<std::string> position = parse(positionString, ",");
+        queens.push_back({ std::stoi(position[0]), std::stoi(position[1]) });
+      }
+
+      inputs.push_back({ std::stoi(numberOfQueens), std::stoi(range), queens });
+    }
+  }
+  inputFile.close();
+
+  for (GAME_NS::GameBoard board : inputs) {
+    board.solve();
     std::cout << std::endl << "===============================================" << std::endl;
   }
 
