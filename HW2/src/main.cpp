8458bbdb7224d79856a2604e8c75ac9a7c182641
Avery Brinkman <averybrinkman@gmail.com>
2023-02-15 12:01:20 -0500

    comments

--- HW2/src/main.cpp
+++ HW2/src/main.cpp
@@ -7,27 +7,41 @@
 
 using namespace GAME_NS;
 
+// Takes a string and splits it into a vector, where delim is the seperator
 std::vector<std::string> parse(std::string& str, std::string delim) {
+  std::vector<std::string> returnList = {};
+
   size_t pos = 0;
   std::string token;
-  std::vector<std::string> out = {};
+
+  // Find the given character in the string
   while ((pos = str.find(delim)) != std::string::npos) {
+    // Split the string
     token = str.substr(0, pos);
-    out.push_back(token);
+    // Add to the list
+    returnList.push_back(token);
+    // Erase that part of the string
     str.erase(0, pos + delim.length());
   }
-  out.push_back(str);
-  return out;
+  // Add the last bit of the string
+  returnList.push_back(str);
+  str.erase(0, str.length());
+
+  return returnList;
 }
 
 int main() {
-  std::string numberOfQueens, range, positionsLine;
   std::vector<GameBoard> inputs = {};
 
-  std::ifstream inputFile("------------PATH/TO/FILE-------------");
+  // Open file
+  std::ifstream inputFile("C:/Users/avery/Documents/C++/AI-Principles-and-Applications/HW2/TestPatterns.txt");
 
+  // Make sure it opened
   if (inputFile.is_open()) {
+    // Read to end
     while (!inputFile.eof()) {
+      // Local vars
+      std::string numberOfQueens, range, positionsLine;
       std::vector<Queen> queens = {};
 
       // Get values from file
@@ -35,17 +49,22 @@ int main() {
       std::getline(inputFile, range);
       std::getline(inputFile, positionsLine);
 
+      // Split the line of positions into vector of positions
       std::vector<std::string> positionsList = parse(positionsLine, " ");
+      // Split each pos into row and col
       for (std::string positionString : positionsList) {
         std::vector<std::string> position = parse(positionString, ",");
+        // Convert row and col to numbers, make a queen, and add it to the list of queens
         queens.push_back({ std::stoi(position[0]), std::stoi(position[1]) });
       }
 
+      // Convert number and range to numbers, create a gameboard, and add it to the list
       inputs.push_back({ std::stoi(numberOfQueens), std::stoi(range), queens });
     }
   }
   inputFile.close();
 
+  // For each game board, solve it
   for (GAME_NS::GameBoard board : inputs) {
     board.solve();
     std::cout << std::endl << "===============================================" << std::endl;
